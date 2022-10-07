3495107010fcaefdef56e227bc2498a552dfddc0
Akshaylakhe24 <akshaylakhe24@gmail.com>
2022-10-07 11:52:02 +0530

    Mokito added

---
+++ src/main/java/com/in28minutes/junit/model/Product.java
@@ -0,0 +1,17 @@
+package com.in28minutes.junit.model;
+
+/**
+ * Product Model API.
+ */
+
+
+public interface Product {
+
+	long getId();
+
+	String getName();
+
+	ProductType getType();
+
+	Amount getAmount();
+}
